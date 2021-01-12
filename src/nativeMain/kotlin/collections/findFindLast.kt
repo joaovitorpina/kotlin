3c8cc1f815ca953aff166fbdbd022ed35aa7a4c8
João Vitor Gonçalves Pina <joaovitorpinag@hotmail.com>
2021-01-12 08:44:09 -0300

    Incluido findFindLast
    Signed-off-by: João Vitor Gonçalves Pina <joaovitorpinag@hotmail.com>

---
+++ src/nativeMain/kotlin/collections/findFindLast.kt
@@ -0,0 +1,10 @@
+package collections
+
+fun findFindLast() {
+    val words = listOf("Lets", "find", "something", "in", "collection", "somehow")
+
+    val first = words.find { it.startsWith("some") }
+    val last = words.findLast { it.startsWith("some") }
+
+    val nothing = words.find { it.contains("nothing") }
+}
