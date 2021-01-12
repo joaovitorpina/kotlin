33622cfc9a0355b246e79d2b18d0feaf090e2728
João Vitor Gonçalves Pina <joaovitorpinag@hotmail.com>
2021-01-12 08:30:38 -0300

    Incluido filter
    Signed-off-by: João Vitor Gonçalves Pina <joaovitorpinag@hotmail.com>

---
+++ src/nativeMain/kotlin/collections/filter.kt
@@ -0,0 +1,9 @@
+package collections
+
+fun filter() {
+    val numbers = listOf(1, -2, 3, -4, 5, -6)
+
+    val positives = numbers.filter { x -> x > 0 }
+
+    val negatives = numbers.filter { it < 0 }
+}
